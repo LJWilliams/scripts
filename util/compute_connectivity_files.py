752094a3a4fe35a9c4e3eee2c74d169b961c6807
Lynne Williams <williams.lynne99@gmail.com>
2025-06-16 13:20:00 -0700

    updating to fix TypeError: 'int' object is not iterable

--- util/compute_connectivity_files.py
+++ util/compute_connectivity_files.py
@@ -111,6 +111,7 @@ def compute_triangle_normals(triangles, vertices):
 
 
 def compute_region_areas_cortex(triangle_areas, vertex_triangles, region_mapping, list_name):
+    print(region_mapping.dtype)
     regions = np.unique(region_mapping)
     region_surface_area = np.zeros((list_name.shape[0], 1))
     if isinstance(triangle_areas, np.ndarray):
@@ -120,6 +121,7 @@ def compute_region_areas_cortex(triangle_areas, vertex_triangles, region_mapping
     #NOTE: Slightly overestimates as it counts overlapping border triangles,
     #      but, not really a problem provided triangle-size << region-size.
     for k in regions:
+        print(k.dtype)
         regs = map(set, avt[region_mapping == k])
         region_triangles = set.union(*regs)
         region_surface_area[k] = triangle_areas[list(region_triangles)].sum()
