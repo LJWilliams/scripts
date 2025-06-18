e853ac82da3638b1079b8875a700d23c5171cbcf
Lynne Williams <williams.lynne99@gmail.com>
2025-06-18 15:38:40 -0700

    fixing index error for zero reference

--- util/subparcel.py
+++ util/subparcel.py
@@ -186,7 +186,9 @@ name_region_cortical = name_region[cortical]
 name_subregion_cortical, idx_subregion_cortical = [], []
 for iregion, region in enumerate(name_region_cortical):
     print(region)
-    name_subregion_cortical.extend([region]*l_nb_subdivisions[l_idx_subdivisions[iregion]])
+    print(l_idx_subdivisions[iregion])
+    print(l_nb_subdivisions[l_idx_subdivis[iregion]])
+    name_subregion_cortical.extend([region]*ionsl_nb_subdivisions[l_idx_subdivis[iregion]])
     print(name_subregion_cortical[-1])
     idx_subregion_cortical.extend([str(i) for i in range(l_nb_subdivisions[l_idx_subdivisions[iregion]])])
 
